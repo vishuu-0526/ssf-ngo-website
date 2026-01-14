5a9459db1cc4b19d695fbf72f6d3a9ad9795a6c1
Vaishnavi Chaudhari <vaishnavi05chaudhari@gmail.com>
2026-01-14 21:04:37 +0530

    Fixed main.jsx merge conflict

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-<<<<<<< HEAD
 import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <StrictMode>
       <App />
     </StrictMode>
-  </BrowserRouter>,
-=======
-
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
->>>>>>> e20eac0 (Updated Website UI and content)
+  </BrowserRouter>
 )
