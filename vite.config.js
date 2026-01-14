4d9ea640977faafa205a2dd69b5a9f5ae0d963de
Vaishnavi Chaudhari <vaishnavi05chaudhari@gmail.com>
2026-01-14 20:54:17 +0530

    Fixed vite config for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,29 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// <<<<<<< HEAD
+// import router from "@tanstack/router-plugin/vite";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),tailwindcss(),router()],
+// =======
+// import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [TanStackRouterVite(), react(), tailwindcss()],
+// >>>>>>> e20eac0 (Updated Website UI and content)
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-<<<<<<< HEAD
-import router from "@tanstack/router-plugin/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),router()],
-=======
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [TanStackRouterVite(), react(), tailwindcss()],
->>>>>>> e20eac0 (Updated Website UI and content)
+  plugins: [react()],
+  server: {
+    port: 5173
+  }
 })
-
 
 
